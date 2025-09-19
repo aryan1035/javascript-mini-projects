e42954543712ff1416030575932fb2fad4c7c067
ary an <aryanashraf.csecu@gmail.com>
2025-09-19 20:57:57 +0600

    done

---
+++ project2/calculator.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+   const height = parseInt(document.querySelector('#height').value)
+   const weight = parseInt(document.querySelector('#weight').value)
+   const results = document.querySelector('#results');
+
+
+   if(height === '' || height < 0 || isNaN(height)){
+    results.innerHTML = "Invalid height or weight"
+
+   }
+
+   else if(weight === '' || height < 0 || isNaN(weight)){
+    results.innerHTML = "Invalid height or weight"
+
+   }
+
+   else{
+     
+   const bmi = (weight / ((height*height)/10000)).toFixed(2);
+
+
+    results.innerHTML = `<span>${bmi}</span>`;
+
+
+   }
+
+})
